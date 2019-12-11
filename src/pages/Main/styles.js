c2f1d264d8eaa8a77dfa93f2336f44190f231a10
evertondg <evertondg@hotmail.com.br>
2019-12-10 23:42:39 -0300

    feat: Listagem de usuários página Main

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -3,7 +3,7 @@ import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled.View`
   flex: 1;
-  padding: 30px;
+  padding: 30px 30px 0;
 `;
 
 export const Form = styled.View`
@@ -30,4 +30,57 @@ export const SubmitButton = styled(RectButton)`
   margin-left: 10px;
   padding: 0 12px;
   background-color: #715991;
+`;
+
+export const List = styled.FlatList.attrs({
+  showsVerticalScrollIndicator: false,
+})`
+  margin-top: 20px;
+`;
+
+export const User = styled.View`
+  align-items: center;
+  margin: 40px 20px 30px;
+  border-radius: 4px;
+  padding: 10px;
+  border: 1px solid #ededed;
+`;
+
+export const Avatar = styled.Image`
+  width: 64px;
+  height: 64px;
+  border-radius: 32px;
+  background: #eee;
+`;
+export const Name = styled.Text`
+  font-size: 14px;
+  color: #333;
+  font-weight: bold;
+  margin-top: 4px;
+  text-align: center;
+`;
+export const Bio = styled.Text.attrs({
+  numberOfLines: 3,
+})`
+  font-size: 13px;
+  line-height: 18px;
+  color: #999;
+  margin-top: 5px;
+  text-align: center;
+`;
+export const ProfileButton = styled(RectButton)`
+  margin-top: 10px;
+  align-self: stretch;
+  border-radius: 4px;
+  background: #7159c1;
+  justify-content: center;
+  align-items: center;
+  height: 40px;
+`;
+export const ProfileButtonText = styled.Text`
+  font-size: 14px;
+  font-weight: bold;
+  color: #fff;
+  text-transform: uppercase;
+  letter-spacing: 1.7px;
 `;
